b2154924a1feadf2842e761d05f21e8bd6b5649d
kaco <hanehdgur@gmail.com>
2020-01-21 17:15:05 +0900

    Initial Commit

---
+++ testCi_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testCi_iOS
+//
+//  Created by kaco on 2020/01/21.
+//  Copyright © 2020 FItpet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
